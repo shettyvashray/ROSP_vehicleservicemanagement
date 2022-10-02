01ac9d5d591c9af2282d8787a9affaf0e7854f62
smith786 <shahsmith100@gmail.com>
2022-10-02 16:35:17 +0530

    js dropdown and charts

--- static/index.js
+++ static/index.js
@@ -30,3 +30,85 @@ function getCookie(cname) {
   }
   return ""
 }
+loadTheme()
+
+function loadTheme() {
+	var theme = getCookie(themeCookieName)
+	body.classList.add(theme === "" ? themeLight : theme)
+}
+
+function switchTheme() {
+	if (body.classList.contains(themeLight)) {
+		body.classList.remove(themeLight)
+		body.classList.add(themeDark)
+		setCookie(themeCookieName, themeDark)
+	} else {
+		body.classList.remove(themeDark)
+		body.classList.add(themeLight)
+		setCookie(themeCookieName, themeLight)
+	}
+}
+
+function collapseSidebar() {
+	body.classList.toggle('sidebar-expand')
+}
+
+window.onclick = function(event) {
+	openCloseDropdown(event)
+}
+
+function closeAllDropdown() {
+	var dropdowns = document.getElementsByClassName('dropdown-expand')
+	for (var i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].classList.remove('dropdown-expand')
+	}
+}
+
+function openCloseDropdown(event) {
+	if (!event.target.matches('.dropdown-toggle')) {
+		// 
+		// Close dropdown when click out of dropdown menu
+		// 
+		closeAllDropdown()
+	} else {
+		var toggle = event.target.dataset.toggle
+		var content = document.getElementById(toggle)
+		if (content.classList.contains('dropdown-expand')) {
+			closeAllDropdown()
+		} else {
+			closeAllDropdown()
+			content.classList.add('dropdown-expand')
+		}
+	}
+}
+
+var ctx = document.getElementById('myChart')
+ctx.height = 500
+ctx.width = 500
+var data = {
+	labels: ['January', 'February', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	datasets: [{
+		fill: false,
+		label: 'Completed',
+		borderColor: successColor,
+		data: [120, 115, 130, 100, 123, 88, 99, 66, 120, 52, 59],
+		borderWidth: 2,
+		lineTension: 0,
+	}, {
+		fill: false,
+		label: 'Issues',
+		borderColor: dangerColor,
+		data: [66, 44, 12, 48, 99, 56, 78, 23, 100, 22, 47],
+		borderWidth: 2,
+		lineTension: 0,
+	}]
+}
+
+var lineChart = new Chart(ctx, {
+	type: 'line',
+	data: data,
+	options: {
+		maintainAspectRatio: false,
+		bezierCurve: false,
+	}
+})
