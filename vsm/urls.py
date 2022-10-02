215a9f20824198472996b7ef412e971961125a30
prathamsolanki-2001 <prathamsolanki2001@gmail.com>
2022-10-02 20:43:04 +0530

    all urls added

--- vsm/urls.py
+++ vsm/urls.py
@@ -60,4 +60,50 @@ urlpatterns = [
 
     path('admin-mechanic', views.admin_mechanic_view,name='admin-mechanic'),
     path('admin-view-mechanic',views.admin_view_mechanic_view,name='admin-view-mechanic'),
+    path('delete-mechanic/<int:pk>', views.delete_mechanic_view,name='delete-mechanic'),
+    path('update-mechanic/<int:pk>', views.update_mechanic_view,name='update-mechanic'),
+    path('admin-add-mechanic',views.admin_add_mechanic_view,name='admin-add-mechanic'),
+    path('admin-approve-mechanic',views.admin_approve_mechanic_view,name='admin-approve-mechanic'),
+    path('approve-mechanic/<int:pk>', views.approve_mechanic_view,name='approve-mechanic'),
+    path('delete-mechanic/<int:pk>', views.delete_mechanic_view,name='delete-mechanic'),
+    path('admin-view-mechanic-salary',views.admin_view_mechanic_salary_view,name='admin-view-mechanic-salary'),
+    path('update-salary/<int:pk>', views.update_salary_view,name='update-salary'),
+
+    path('admin-mechanic-attendance', views.admin_mechanic_attendance_view,name='admin-mechanic-attendance'),
+    path('admin-take-attendance', views.admin_take_attendance_view,name='admin-take-attendance'),
+    path('admin-view-attendance', views.admin_view_attendance_view,name='admin-view-attendance'),
+    path('admin-feedback', views.admin_feedback_view,name='admin-feedback'),
+
+    path('admin-report', views.admin_report_view,name='admin-report'),
+
+    path('mechanic-dashboard', views.mechanic_dashboard_view,name='mechanic-dashboard'),
+    path('mechanic-work-assigned', views.mechanic_work_assigned_view,name='mechanic-work-assigned'),
+    path('mechanic-update-status/<int:pk>', views.mechanic_update_status_view,name='mechanic-update-status'),
+    path('mechanic-feedback', views.mechanic_feedback_view,name='mechanic-feedback'),
+    path('mechanic-salary', views.mechanic_salary_view,name='mechanic-salary'),
+    path('mechanic-profile', views.mechanic_profile_view,name='mechanic-profile'),
+    path('edit-mechanic-profile', views.edit_mechanic_profile_view,name='edit-mechanic-profile'),
+
+    path('mechanic-attendance', views.mechanic_attendance_view,name='mechanic-attendance'),
+
+
+
+    path('customer-dashboard', views.customer_dashboard_view,name='customer-dashboard'),
+    path('customer-request', views.customer_request_view,name='customer-request'),
+    path('customer-add-request',views.customer_add_request_view,name='customer-add-request'),
+
+    path('customer-profile', views.customer_profile_view,name='customer-profile'),
+    path('edit-customer-profile', views.edit_customer_profile_view,name='edit-customer-profile'),
+    path('customer-feedback', views.customer_feedback_view,name='customer-feedback'),
+    path('customer-invoice', views.customer_invoice_view,name='customer-invoice'),
+    path('customer-view-request',views.customer_view_request_view,name='customer-view-request'),
+    path('customer-delete-request/<int:pk>', views.customer_delete_request_view,name='customer-delete-request'),
+    path('customer-view-approved-request',views.customer_view_approved_request_view,name='customer-view-approved-request'),
+    path('customer-view-approved-request-invoice',views.customer_view_approved_request_invoice_view,name='customer-view-approved-request-invoice'),
+
+    path('afterlogin', views.afterlogin_view,name='afterlogin'),
+    path('logout', LogoutView.as_view(template_name='vehicle/index.html'),name='logout'),
+
+    path('aboutus', views.aboutus_view),
+    path('contactus', views.contactus_view),
 ]
