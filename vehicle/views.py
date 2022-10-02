1558129f73c15b38d3d484d4060346b85c8165fc
shettyvashray <shettyvashray@gmail.com>
2022-10-02 21:02:09 +0530

    Styles and views updated

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,3 +1,348 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from . import forms,models
+from django.db.models import Sum
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.conf import settings
+from django.db.models import Q
 
-# Create your views here.
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'vehicle/index.html')
+
+
+#for showing signup/login button for customer
+def customerclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'vehicle/customerclick.html')
+
+#for showing signup/login button for mechanics
+def mechanicsclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'vehicle/mechanicsclick.html')
+
+
+#for showing signup/login button for ADMIN(by sumit)
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return HttpResponseRedirect('adminlogin')
+
+
+def customer_signup_view(request):
+    userForm=forms.CustomerUserForm()
+    customerForm=forms.CustomerForm()
+    mydict={'userForm':userForm,'customerForm':customerForm}
+    if request.method=='POST':
+        userForm=forms.CustomerUserForm(request.POST)
+        customerForm=forms.CustomerForm(request.POST,request.FILES)
+        if userForm.is_valid() and customerForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            customer=customerForm.save(commit=False)
+            customer.user=user
+            customer.save()
+            my_customer_group = Group.objects.get_or_create(name='CUSTOMER')
+            my_customer_group[0].user_set.add(user)
+        return HttpResponseRedirect('customerlogin')
+    return render(request,'vehicle/customersignup.html',context=mydict)
+
+
+def mechanic_signup_view(request):
+    userForm=forms.MechanicUserForm()
+    mechanicForm=forms.MechanicForm()
+    mydict={'userForm':userForm,'mechanicForm':mechanicForm}
+    if request.method=='POST':
+        userForm=forms.MechanicUserForm(request.POST)
+        mechanicForm=forms.MechanicForm(request.POST,request.FILES)
+        if userForm.is_valid() and mechanicForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            mechanic=mechanicForm.save(commit=False)
+            mechanic.user=user
+            mechanic.save()
+            my_mechanic_group = Group.objects.get_or_create(name='MECHANIC')
+            my_mechanic_group[0].user_set.add(user)
+        return HttpResponseRedirect('mechaniclogin')
+    return render(request,'vehicle/mechanicsignup.html',context=mydict)
+
+
+#for checking user customer, mechanic or admin(by sumit)
+def is_customer(user):
+    return user.groups.filter(name='CUSTOMER').exists()
+def is_mechanic(user):
+    return user.groups.filter(name='MECHANIC').exists()
+
+
+def afterlogin_view(request):
+    if is_customer(request.user):
+        return redirect('customer-dashboard')
+    elif is_mechanic(request.user):
+        accountapproval=models.Mechanic.objects.all().filter(user_id=request.user.id,status=True)
+        if accountapproval:
+            return redirect('mechanic-dashboard')
+        else:
+            return render(request,'vehicle/mechanic_wait_for_approval.html')
+    else:
+        return redirect('admin-dashboard')
+
+
+
+#============================================================================================
+# ADMIN RELATED views start
+#============================================================================================
+
+@login_required(login_url='adminlogin')
+def admin_dashboard_view(request):
+    enquiry=models.Request.objects.all().order_by('-id')
+    customers=[]
+    for enq in enquiry:
+        customer=models.Customer.objects.get(id=enq.customer_id)
+        customers.append(customer)
+    dict={
+    'total_customer':models.Customer.objects.all().count(),
+    'total_mechanic':models.Mechanic.objects.all().count(),
+    'total_request':models.Request.objects.all().count(),
+    'total_feedback':models.Feedback.objects.all().count(),
+    'data':zip(customers,enquiry),
+    }
+    return render(request,'vehicle/admin_dashboard.html',context=dict)
+
+
+@login_required(login_url='adminlogin')
+def admin_customer_view(request):
+    return render(request,'vehicle/admin_customer.html')
+
+@login_required(login_url='adminlogin')
+def admin_view_customer_view(request):
+    customers=models.Customer.objects.all()
+    return render(request,'vehicle/admin_view_customer.html',{'customers':customers})
+
+
+@login_required(login_url='adminlogin')
+def delete_customer_view(request,pk):
+    customer=models.Customer.objects.get(id=pk)
+    user=models.User.objects.get(id=customer.user_id)
+    user.delete()
+    customer.delete()
+    return redirect('admin-view-customer')
+
+
+@login_required(login_url='adminlogin')
+def update_customer_view(request,pk):
+    customer=models.Customer.objects.get(id=pk)
+    user=models.User.objects.get(id=customer.user_id)
+    userForm=forms.CustomerUserForm(instance=user)
+    customerForm=forms.CustomerForm(request.FILES,instance=customer)
+    mydict={'userForm':userForm,'customerForm':customerForm}
+    if request.method=='POST':
+        userForm=forms.CustomerUserForm(request.POST,instance=user)
+        customerForm=forms.CustomerForm(request.POST,request.FILES,instance=customer)
+        if userForm.is_valid() and customerForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            customerForm.save()
+            return redirect('admin-view-customer')
+    return render(request,'vehicle/update_customer.html',context=mydict)
+
+
+@login_required(login_url='adminlogin')
+def admin_add_customer_view(request):
+    userForm=forms.CustomerUserForm()
+    customerForm=forms.CustomerForm()
+    mydict={'userForm':userForm,'customerForm':customerForm}
+    if request.method=='POST':
+        userForm=forms.CustomerUserForm(request.POST)
+        customerForm=forms.CustomerForm(request.POST,request.FILES)
+        if userForm.is_valid() and customerForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            customer=customerForm.save(commit=False)
+            customer.user=user
+            customer.save()
+            my_customer_group = Group.objects.get_or_create(name='CUSTOMER')
+            my_customer_group[0].user_set.add(user)
+        return HttpResponseRedirect('/admin-view-customer')
+    return render(request,'vehicle/admin_add_customer.html',context=mydict)
+
+
+@login_required(login_url='adminlogin')
+def admin_view_customer_enquiry_view(request):
+    enquiry=models.Request.objects.all().order_by('-id')
+    customers=[]
+    for enq in enquiry:
+        customer=models.Customer.objects.get(id=enq.customer_id)
+        customers.append(customer)
+    return render(request,'vehicle/admin_view_customer_enquiry.html',{'data':zip(customers,enquiry)})
+
+
+@login_required(login_url='adminlogin')
+def admin_view_customer_invoice_view(request):
+    enquiry=models.Request.objects.values('customer_id').annotate(Sum('cost'))
+    print(enquiry)
+    customers=[]
+    for enq in enquiry:
+        print(enq)
+        customer=models.Customer.objects.get(id=enq['customer_id'])
+        customers.append(customer)
+    return render(request,'vehicle/admin_view_customer_invoice.html',{'data':zip(customers,enquiry)})
+
+@login_required(login_url='adminlogin')
+def admin_mechanic_view(request):
+    return render(request,'vehicle/admin_mechanic.html')
+
+
+@login_required(login_url='adminlogin')
+def admin_approve_mechanic_view(request):
+    mechanics=models.Mechanic.objects.all().filter(status=False)
+    return render(request,'vehicle/admin_approve_mechanic.html',{'mechanics':mechanics})
+
+@login_required(login_url='adminlogin')
+def approve_mechanic_view(request,pk):
+    mechanicSalary=forms.MechanicSalaryForm()
+    if request.method=='POST':
+        mechanicSalary=forms.MechanicSalaryForm(request.POST)
+        if mechanicSalary.is_valid():
+            mechanic=models.Mechanic.objects.get(id=pk)
+            mechanic.salary=mechanicSalary.cleaned_data['salary']
+            mechanic.status=True
+            mechanic.save()
+        else:
+            print("form is invalid")
+        return HttpResponseRedirect('/admin-approve-mechanic')
+    return render(request,'vehicle/admin_approve_mechanic_details.html',{'mechanicSalary':mechanicSalary})
+
+
+@login_required(login_url='adminlogin')
+def delete_mechanic_view(request,pk):
+    mechanic=models.Mechanic.objects.get(id=pk)
+    user=models.User.objects.get(id=mechanic.user_id)
+    user.delete()
+    mechanic.delete()
+    return redirect('admin-approve-mechanic')
+
+
+@login_required(login_url='adminlogin')
+def admin_add_mechanic_view(request):
+    userForm=forms.MechanicUserForm()
+    mechanicForm=forms.MechanicForm()
+    mechanicSalary=forms.MechanicSalaryForm()
+    mydict={'userForm':userForm,'mechanicForm':mechanicForm,'mechanicSalary':mechanicSalary}
+    if request.method=='POST':
+        userForm=forms.MechanicUserForm(request.POST)
+        mechanicForm=forms.MechanicForm(request.POST,request.FILES)
+        mechanicSalary=forms.MechanicSalaryForm(request.POST)
+        if userForm.is_valid() and mechanicForm.is_valid() and mechanicSalary.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            mechanic=mechanicForm.save(commit=False)
+            mechanic.user=user
+            mechanic.status=True
+            mechanic.salary=mechanicSalary.cleaned_data['salary']
+            mechanic.save()
+            my_mechanic_group = Group.objects.get_or_create(name='MECHANIC')
+            my_mechanic_group[0].user_set.add(user)
+            return HttpResponseRedirect('admin-view-mechanic')
+        else:
+            print('problem in form')
+    return render(request,'vehicle/admin_add_mechanic.html',context=mydict)
+
+
+@login_required(login_url='adminlogin')
+def admin_view_mechanic_view(request):
+    mechanics=models.Mechanic.objects.all()
+    return render(request,'vehicle/admin_view_mechanic.html',{'mechanics':mechanics})
+
+
+@login_required(login_url='adminlogin')
+def delete_mechanic_view(request,pk):
+    mechanic=models.Mechanic.objects.get(id=pk)
+    user=models.User.objects.get(id=mechanic.user_id)
+    user.delete()
+    mechanic.delete()
+    return redirect('admin-view-mechanic')
+
+
+@login_required(login_url='adminlogin')
+def update_mechanic_view(request,pk):
+    mechanic=models.Mechanic.objects.get(id=pk)
+    user=models.User.objects.get(id=mechanic.user_id)
+    userForm=forms.MechanicUserForm(instance=user)
+    mechanicForm=forms.MechanicForm(request.FILES,instance=mechanic)
+    mydict={'userForm':userForm,'mechanicForm':mechanicForm}
+    if request.method=='POST':
+        userForm=forms.MechanicUserForm(request.POST,instance=user)
+        mechanicForm=forms.MechanicForm(request.POST,request.FILES,instance=mechanic)
+        if userForm.is_valid() and mechanicForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            mechanicForm.save()
+            return redirect('admin-view-mechanic')
+    return render(request,'vehicle/update_mechanic.html',context=mydict)
+
+@login_required(login_url='adminlogin')
+def admin_view_mechanic_salary_view(request):
+    mechanics=models.Mechanic.objects.all()
+    return render(request,'vehicle/admin_view_mechanic_salary.html',{'mechanics':mechanics})
+
+@login_required(login_url='adminlogin')
+def update_salary_view(request,pk):
+    mechanicSalary=forms.MechanicSalaryForm()
+    if request.method=='POST':
+        mechanicSalary=forms.MechanicSalaryForm(request.POST)
+        if mechanicSalary.is_valid():
+            mechanic=models.Mechanic.objects.get(id=pk)
+            mechanic.salary=mechanicSalary.cleaned_data['salary']
+            mechanic.save()
+        else:
+            print("form is invalid")
+        return HttpResponseRedirect('/admin-view-mechanic-salary')
+    return render(request,'vehicle/admin_approve_mechanic_details.html',{'mechanicSalary':mechanicSalary})
+
+
+@login_required(login_url='adminlogin')
+def admin_request_view(request):
+    return render(request,'vehicle/admin_request.html')
+
+@login_required(login_url='adminlogin')
+def admin_view_request_view(request):
+    enquiry=models.Request.objects.all().order_by('-id')
+    customers=[]
+    for enq in enquiry:
+        customer=models.Customer.objects.get(id=enq.customer_id)
+        customers.append(customer)
+    return render(request,'vehicle/admin_view_request.html',{'data':zip(customers,enquiry)})
+
+
+@login_required(login_url='adminlogin')
+def change_status_view(request,pk):
+    adminenquiry=forms.AdminApproveRequestForm()
+    if request.method=='POST':
+        adminenquiry=forms.AdminApproveRequestForm(request.POST)
+        if adminenquiry.is_valid():
+            enquiry_x=models.Request.objects.get(id=pk)
+            enquiry_x.mechanic=adminenquiry.cleaned_data['mechanic']
+            enquiry_x.cost=adminenquiry.cleaned_data['cost']
+            enquiry_x.status=adminenquiry.cleaned_data['status']
+            enquiry_x.save()
+        else:
+            print("form is invalid")
+        return HttpResponseRedirect('/admin-view-request')
+    return render(request,'vehicle/admin_approve_request_details.html',{'adminenquiry':adminenquiry})
+
+
+@login_required(login_url='adminlogin')
+def admin_delete_request_view(request,pk):
+    requests=models.Request.objects.get(id=pk)
+    requests.delete()
+    return redirect('admin-view-request')
